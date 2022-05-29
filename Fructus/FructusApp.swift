7bb689a31dbc339784ad3d16929c7322dab3cb9b
sunilLDT <sunil@ldttechnology.com>
2022-05-29 13:00:24 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Apple on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
